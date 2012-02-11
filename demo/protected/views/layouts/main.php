b54537536777e7680262a80410fab6b16c4ef0ff
Crisu83 <none@none>
2012-02-11 13:26:20 +0200

    Fixed a few things in the demo Facebook integration.
    --HG--
    branch : bootstrap2

--- demo/protected/views/layouts/main.php
+++ demo/protected/views/layouts/main.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml" xml:lang="en" lang="en">
+<html>
 <head prefix="og: http://ogp.me/ns# <?php echo Yii::app()->fb->appNamespace; ?>: http://ogp.me/ns/apps/<?php echo Yii::app()->fb->appNamespace; ?>#">
 	<?php Yii::app()->controller->widget('ext.seo.widgets.SeoHead', array(
 		'defaultDescription'=>Yii::app()->params['appDescription'],
@@ -29,14 +29,25 @@
 <body id="top">
 
 <div id="fb-root"></div>
-<script type="text/javascript">
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : <?php echo Yii::app()->fb->appID; ?>, // App ID
+			status     : true, // check login status
+			cookie     : true, // enable cookies to allow the server to access the session
+			xfbml      : true  // parse XFBML
+		});
+
+		// Additional initialization code here
+	};
+
+	// Load the SDK Asynchronously
+	(function(d){
+		var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+		js = d.createElement('script'); js.id = id; js.async = true;
+		js.src = "//connect.facebook.net/en_US/all.js";
+		d.getElementsByTagName('head')[0].appendChild(js);
+	}(document));
 </script>
 
 <?php $this->widget('BootNavbar',array(
