f9ee399055d649f1c855d1d199a97303ef6b4124
Crisu83 <none@none>
2012-02-13 22:50:55 +0200

    fixed the twitter link to be absolute (doh)
    --HG--
    branch : bootstrap-2.0.1

--- demo/protected/views/layouts/main.php
+++ demo/protected/views/layouts/main.php
@@ -60,7 +60,7 @@
 				array('label'=>'Demo', 'url'=>Yii::app()->homeUrl,
 						'active'=>Yii::app()->controller->id === 'site' && Yii::app()->controller->action->id === 'index'),
 				array('label'=>'Setup', 'url'=>array('site/setup')),
-				array('label'=>'@Crisu83', 'url'=>array('http://www.twitter.com/Crisu83')),
+				array('label'=>'@Crisu83', 'url'=>'http://www.twitter.com/Crisu83', 'itemOptions'=>array('target'=>'_blank')),
 			),
 		),
 		'
