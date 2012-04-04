6510115dada24ef3a87ef33935c07909cdb270a7
Crisu83 <none@none>
2012-04-05 01:27:33 +0300

    updated the demo
    --HG--
    branch : 1.0.0-wip

--- demo/protected/views/site/index.php
+++ demo/protected/views/site/index.php
@@ -335,7 +335,7 @@ Yii::app()->user->setFlash('error', '<strong>Oh snap!</strong> Change a few thin
 	'type'=>'tabs',
 	'placement'=>'below', // 'above', 'right', 'below' or 'left'
 	'tabs'=>array(
-		array('label'=>'Section 1', 'content'=>'<p>I\'m in Section 1.</p>'),
+		array('label'=>'Section 1', 'content'=>'<p>I\'m in Section 1.</p>', 'active'=>true),
 		array('label'=>'Section 2', 'content'=>'<p>Howdy, I\'m in Section 2.</p>'),
 		array('label'=>'Section 3', 'content'=>'<p>What up girl, this is Section 3.</p>'),
 	),
